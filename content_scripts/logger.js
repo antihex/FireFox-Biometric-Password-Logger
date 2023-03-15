d630a10a7999fa151c8345afe8043ea2d2279c3a
AntiHex <zmconger@gmail.com>
2023-03-15 16:44:00 -0400

    added key down and up

--- content_scripts/logger.js
+++ content_scripts/logger.js
@@ -1,6 +1,11 @@
 console.log("Extension background script loaded!");
 
 document.addEventListener("keydown", function(event) {
-    // Log the key code to the console
-    console.log("Key pressed:", event.keyCode);
-  });
+  // Log the key code to the console
+  console.log("Key pressed:", event.keyCode);
+});
+
+document.addEventListener("keyup", function(event) {
+  // Log the key code to the console
+  console.log("Key released:", event.keyCode);
+});
